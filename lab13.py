1ff3a9de52747baf8c11a1f572914a4cc50b8f65
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-12-11 18:46:30 +0400

    Add files via upload

--- lab13.py
+++ lab13.py
@@ -1,4 +1,4 @@
-# Определить количество пассажиров, севших в порту Шербур,
+# Определить количество женщин, севших в порту Шербур,
 # в возрастном интервале средний возраст +- 5 лет и сколько из них выжило
 
 import csv
@@ -18,7 +18,7 @@ min_age = 200
 
 for i in rows:
     if i[1] == "1" or i[1] == "0":
-        if i[5] != "" and i[5] != " ":
+        if i[5] != "" and i[5] != " " and i[4] == "female":  # количество женщин
             if float(i[5]) > max_age:
                 max_age = float(i[5])
             elif float(i[5]) < min_age:
@@ -28,15 +28,15 @@ median_age = (max_age + min_age) / 2
 
 for i in rows:
     if i[1] == "1" or i[1] == "0":
-        if i[5] != "" and i[5] != " ":
-            if abs(median_age - float(i[5])) <= 5:
+        if i[5] != "" and i[5] != " " and i[4] == "female":  # количество женщин
+            if abs(median_age - float(i[5])) <= 7:
                 count_all += 1
 for i in rows:
     if i[1] == "1" or i[1] == "0":
-        if i[11] == "C":  # Проверка на соответствие условию C = Cherbourg Q = Queenstown S = Southampton
+        if i[11] == "C" and i[4] == "female":  # Проверка на соответствие условию C = Cherbourg Q = Queenstown S = Southampton и полу пассажира
             if i[5] != "" and i[5] != " ":  # Проверка на пустую строку
                 count_all += 1
                 if i[1] == "1":
                     count_survivor += 1
 
-print(f"Пассажиров, севших в порту Шейбур : {count_all}\nИз них выжило: {count_survivor}")
+print(f"Женщин, севших в порту Шербур : {count_all}\nИз них выжило: {count_survivor}")
