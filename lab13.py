1135feadb04ba4f51c2654ce9ee29f2881756274
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-12-12 18:10:45 +0400

    Add files via upload

--- lab13.py
+++ lab13.py
@@ -26,15 +26,11 @@ median_age = (max_age + min_age) / 2
 
 for i in rows:
     if i[1] == "1" or i[1] == "0":
-        if i[5] != "" and i[5] != " " and i[4] == "female":
-            if abs(median_age - float(i[5])) <= 5:
-                count_all += 1
-for i in rows:
-    if i[1] == "1" or i[1] == "0":
         if i[11] == "C" and i[4] == "female":  # Проверка на соответствие условию C = Cherbourg Q = Queenstown S = Southampton и полу пассажира
             if i[5] != "" and i[5] != " ":  # Проверка на пустую строку
-                count_all += 1
-                if i[1] == "1":
-                    count_survivor += 1
+                if abs(median_age - float(i[5])) <= 5:
+                    count_all += 1
+                    if i[1] == "1":
+                        count_survivor += 1
 
 print(f"Женщин, севших в порту Шербур : {count_all}\nИз них выжило: {count_survivor}")
